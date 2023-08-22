7b6438f83d4ec328a4aeced264652fcedf587dd2
Guobin <qracle@126.com>
2023-08-22 10:11:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("webapp1..."))
+		w.Write([]byte("webapp1"))
 	})
 	http.ListenAndServe(":3000", nil)
 }
